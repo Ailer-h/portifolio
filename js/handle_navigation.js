62b0e909d97e29a99f779fbf97818ee50ef5e3ee
Ailer-h <henriqueailer2@gmail.com>
2025-07-13 17:46:22 -0300

    Updated a few sizes in design and fixed the navigator.

--- js/handle_navigation.js
+++ js/handle_navigation.js
@@ -1,5 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
     load_navigation("#navigator")
+
+    this.addEventListener("scrollend", () => {
+        set_navigator(get_current_section())
+    })
 });
 
 //Hardcoded but it's gonna be replaced with a database call
@@ -12,12 +16,39 @@ const navigation_data = {
     ],
 
     "titles": {
-        "home": {"pt": "Home", "en": "Home"},
-        "exp": {"pt": "Experiência", "en": "Experience"},
-        "projects": {"pt": "Projetos", "en": "Projects"},
-        "edu": {"pt": "Educação", "en": "Education"}
+        "home": { "pt": "Home", "en": "Home" },
+        "exp": { "pt": "Experiência", "en": "Experience" },
+        "projects": { "pt": "Projetos", "en": "Projects" },
+        "edu": { "pt": "Educação", "en": "Education" }
 
     }
+}
+
+function inSection(element) {
+    const item = element.getBoundingClientRect();
+
+    return (
+        item.top >= 0 &&
+        item.left >= 0 &&
+        item.bottom <= (
+            window.innerHeight ||
+            document.documentElement.clientHeight) &&
+        item.right <= (
+            window.innerWidth ||
+            document.documentElement.clientWidth)
+    );
+}
+
+function get_current_section() {
+
+    const sections = navigation_data['sections']
+
+    for (const section of sections) {
+        if (inSection(document.getElementById("section_" + section))) {
+            return section
+        }
+    }
+
 }
 
 function load_navigation(navigator_id) {
@@ -27,7 +58,7 @@ function load_navigation(navigator_id) {
     const sections = navigation_data['sections'];
 
     sections.forEach(section_id => {
-        
+
         let section_title = titles[section_id][lang];
 
         navigator_html += `<li id='li_${section_id}' onclick="goToSection('${section_id}')">
@@ -38,24 +69,30 @@ function load_navigation(navigator_id) {
     $(navigator_id).html(navigator_html)
 
     $("#page_navigator").html("<div class='dot'></div>".repeat(sections.length))
-    $($(".dot").toArray()[0]).css({"opacity": "1"})
+    $($(".dot").toArray()[0]).css({ "opacity": "1" })
 
-    $("#li_" + sections[0]).css({"font-weight": "bold"})
+    $("#li_" + sections[0]).css({ "font-weight": "bold" })
 
+}
+
+function set_navigator(section) {
+    const sections = navigation_data['sections'];
+
+    $("#navigator li").css({ "font-weight": "300" })
+    $("#li_" + section).css({ "font-weight": "bold" })
+
+    $(".dot").css({ "opacity": "0.5" });
+    $($(".dot").toArray()[sections.indexOf(section)]).css({ "opacity": "1" })
 }
 
 function goToSection(section) {
 
     let section_id = "#section_" + section;
-    const sections = navigation_data['sections'];
 
     document.querySelector(section_id).scrollIntoView({
         behavior: "smooth"
     });
 
-    $("#navigator li").css({"font-weight": "300"})
-    $("#li_" + section).css({"font-weight": "bold"})
+    set_navigator(section)
 
-    $(".dot").css({"opacity": "0.5"});
-    $($(".dot").toArray()[sections.indexOf(section)]).css({"opacity": "1"})
 }
