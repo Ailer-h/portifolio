8ded777333a685dd84ac7a1f52fcba0721c8048a
Ailer-h <henriqueailer2@gmail.com>
2026-05-15 14:23:48 -0300

    Initial Commit. Started the sidebar.

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Routes, Route } from 'react-router-dom'
+import Sidebar from './components/Sidebar'
+
+function App() {
+
+  return <>
+
+    <div className="grid">
+      <Sidebar/>
+      <div></div>
+    </div>
+
+  </>
+}
+
+export default App
